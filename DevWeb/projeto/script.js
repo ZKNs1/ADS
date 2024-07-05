a59fbaee0709cc8f8ddcf5fe60f722a55b8a5b87
ZKNs1 <msaori40@gmail.com>
2024-07-05 00:25:13 -0300

    Adjusted submodules

---
+++ DevWeb/projeto/script.js
@@ -0,0 +1,11 @@
+document.getElementByClass("mat").addEventListener("click", function() {
+    window.location.href = "https://www.roblox.com/"; 
+});
+
+document.getElementByClass("inss").addEventListener("click", function() {
+    window.location.href = "https://www.roblox.com/"; 
+});
+
+document.getElementByClass("script").addEventListener("click", function() {
+    window.location.href = "https://www.roblox.com/"; 
+});
